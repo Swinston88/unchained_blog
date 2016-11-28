57c526b732796509423666301c0d72cc9f15ceb4
hanrattyjen <hanratty.jen@gmail.com>
2016-11-28 22:34:34 +0000

    Changed static folder path in STATICFILES_DIRS

--- mysite/settings.py
+++ mysite/settings.py
@@ -202,7 +202,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'blog/static'),
 )
 
 # Simplified static file serving.
